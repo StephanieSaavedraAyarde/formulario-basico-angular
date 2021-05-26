f314271cdcc8c9c1afd001809a9351ed4298c984
StephanieSaavedraAyarde <stephanie.saavedra@ucb.edu.bo>
2021-05-26 17:02:56 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   stepCount: number = 1;
-  isFormSubmitted:boolean = false;
+  isFormSubmitted: boolean = false;
   formGroup1: any;
   formGroup2: any;
   formGroup3: any;
   formsValidationObj: any;
+  salario;
+
+  public form: FormGroup;
+  public contactList: FormArray;
+
+  // returns all form groups under contacts
+  get contactFormGroup() {
+    return this.form.get('contacts') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) {}
 
@@ -20,19 +29,32 @@ export class AppComponent implements OnInit {
     this.stepCount = 1;
     this.isFormSubmitted = false;
     this.formGroup1 = this.fb.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required]
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
     });
 
     this.formGroup2 = this.fb.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required]
+      nombre: ['', Validators.required],
+      paterno: ['', Validators.required],
+      materno: ['', Validators.required],
+      ci: ['', Validators.required],
+      fecha: ['', Validators.required],
+      empleado: ['', Validators.required],
+      area: ['', Validators.required],
+      profesion: ['', Validators.required],
+      antiguedad: ['', Validators.required],
+      salario: ['', Validators.required],
+      mes: ['', Validators.required]
     });
 
-    this.formGroup3 = this.fb.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required]
+    this.form = this.fb.group({
+      name: [null, Validators.compose([Validators.required])],
+      organization: [null],
+      contacts: this.fb.array([this.createContact()])
     });
+
+    // set contactlist to this field
+    this.contactList = this.form.get('contacts') as FormArray;
 
     this.formsValidationObj = {
       form1Valid: false,
@@ -89,9 +111,60 @@ export class AppComponent implements OnInit {
   }
 
   onPrevClick() {
-    if(this.stepCount > 1) {
+    if (this.stepCount > 1) {
       this.isFormSubmitted = false;
       this.stepCount -= 1;
     }
+  }
+
+  createContact(): FormGroup {
+    return this.fb.group({
+      type: ['email', Validators.compose([Validators.required])], // i.e Email, Phone
+      name: [null, Validators.compose([Validators.required])], // i.e. Home, Office
+      value: [null, Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  // add a contact form group
+  addContact() {
+    this.contactList.push(this.createContact());
+  }
+
+  // remove contact from group
+  removeContact(index) {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    this.contactList.removeAt(index);
+  }
+
+  // triggered to change validation of value field type
+  changedFieldType(index) {
+    let validators = null;
+
+    if (this.getContactsFormGroup(index).controls['type'].value === 'email') {
+      validators = Validators.compose([Validators.required, Validators.email]);
+    } else {
+      validators = Validators.compose([
+        Validators.required,
+        Validators.pattern(new RegExp('^\\+[0-9]?()[0-9](\\d[0-9]{9})$')) // pattern for validating international phone number
+      ]);
+    }
+
+    this.getContactsFormGroup(index).controls['value'].setValidators(
+      validators
+    );
+
+    this.getContactsFormGroup(index).controls['value'].updateValueAndValidity();
+  }
+
+  // get the formgroup under contacts form array
+  getContactsFormGroup(index): FormGroup {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    const formGroup = this.contactList.controls[index] as FormGroup;
+    return formGroup;
+  }
+
+  // method triggered when form is submitted
+  submit() {
+    console.log(this.form.value);
   }
 }
